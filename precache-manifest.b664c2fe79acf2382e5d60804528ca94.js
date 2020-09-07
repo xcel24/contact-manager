a34bcbd55deb44d097e1240035dd2fe164382e5f
xcel24 <prateekgoswami1996@gmail.com>
2020-09-07 19:13:42 +0530

    Updates

---
+++ precache-manifest.b664c2fe79acf2382e5d60804528ca94.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5c043480c66b1bae4eaeff1976808df",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "3ca8c2923b8eccf87dd0",
+    "url": "/contactmanager/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "d181d151ad5c66cf5d67",
+    "url": "/contactmanager/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "3ca8c2923b8eccf87dd0",
+    "url": "/contactmanager/static/js/2.d6b5a83f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanager/static/js/2.d6b5a83f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d181d151ad5c66cf5d67",
+    "url": "/contactmanager/static/js/main.f1ff0ece.chunk.js"
+  },
+  {
+    "revision": "e155b645805d8202c894",
+    "url": "/contactmanager/static/js/runtime-main.8912a38d.js"
+  }
+]);
